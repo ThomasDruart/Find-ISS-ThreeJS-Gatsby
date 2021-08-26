59c7d2f435408020d39cfe0ee2658cd3865de00f
Thomas Druart <thomas.druart.59@gmail.com>
2021-08-26 21:50:22 +0200

    iss position WIP

---
+++ src/components/three-iss.js
@@ -0,0 +1,11 @@
+import React from "react";
+import * as THREE from "three";
+import axios from "axios";
+
+const ThreeIss = () => {
+  const [issLocation, setIssLocation] = useState(null);
+
+  return <></>;
+};
+
+export default ThreeIss;
