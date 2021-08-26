9b723a92b2082f2e1a013e9568e9695a5330f17c
Thomas Druart <thomas.druart.59@gmail.com>
2021-08-26 22:05:25 +0200

    import getVertex function

--- src/components/three-iss.js
+++ src/components/three-iss.js
@@ -2,6 +2,8 @@ import React from "react";
 import * as THREE from "three";
 import axios from "axios";
 
+import getVertex from "../utils/getVertex";
+
 const ThreeIss = () => {
   const [issLocation, setIssLocation] = useState(null);
 
