41193f5616fdf139a91d8063eeda105a9bd6a6a4
Thomas Druart <thomas.druart.59@gmail.com>
2021-09-05 14:55:09 +0200

    WIP

--- src/components/three-iss.js
+++ src/components/three-iss.js
@@ -7,6 +7,16 @@ import getVertex from "../utils/getVertex";
 const ThreeIss = () => {
   const [issLocation, setIssLocation] = useState(null);
 
+  const poll = async () => {
+    try {
+      const res = await axios.get("/api/get-iss-location");
+      setIssLocation(res.data.iss_now);
+    } catch (err) {
+      console.log(err);
+      throw new Error();
+    }
+  };
+
   return <></>;
 };
 
