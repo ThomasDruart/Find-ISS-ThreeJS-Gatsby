1528542c7b797a4abe0389a4d0450cb348b50995
Thomas Druart <thomas.druart.59@gmail.com>
2021-09-05 15:54:22 +0200

    WIP

--- src/components/three-iss.js
+++ src/components/three-iss.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import * as THREE from "three";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import getVertex from "../utils/getVertex";
@@ -27,7 +26,18 @@ const ThreeIss = () => {
     return () => clearInterval(pollInterval);
   }, []);
 
-  return <></>;
+  return (
+    <>
+      {issLocation && (
+        <mesh
+          position={getVertex(issLocation.latitude, issLocation.longitude, 120)}
+        >
+          <sphereGeometry args={[2]} />
+          <meshBasicMaterial color="#000000" />
+        </mesh>
+      )}
+    </>
+  );
 };
 
 export default ThreeIss;
