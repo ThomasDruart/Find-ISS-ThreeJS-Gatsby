a28128172da705bcaa93603d235aae5b5b900842
Thomas Druart <thomas.druart.59@gmail.com>
2021-09-05 14:54:58 +0200

    vertex function

--- src/utils/getVertex.js
+++ src/utils/getVertex.js
@@ -1,5 +1,14 @@
 import * as THREE from "three";
 
-const getVertex = () => {};
+const getVertex = (latidude, longitude, radius) => {
+  const vector = new THREE.Vector3().setFromSpherical(
+    new THREE.Spherical(
+      radius,
+      THREE.MathUtils.degToRad(90 - latidude),
+      THREE.MathUtils.degToRad(longitude)
+    )
+  );
+  return vector;
+};
 
 export default getVertex;
