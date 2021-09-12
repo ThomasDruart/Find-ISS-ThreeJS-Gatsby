dcc69efaed09b12896c1dd20d34276578cdda082
Thomas Druart <thomas.druart.59@gmail.com>
2021-09-12 16:42:17 +0200

    sphere color

--- src/components/three-sphere.js
+++ src/components/three-sphere.js
@@ -4,7 +4,7 @@ const ThreeSphere = () => {
   return (
     <mesh>
       <sphereGeometry args={[100, 32, 32]} />
-      <meshBasicMaterial color="#f7f7f7" transparent={true} opacity={0.6} />
+      <meshBasicMaterial color="#C3DBFF" transparent={true} opacity={0.6} />
     </mesh>
   );
 };
